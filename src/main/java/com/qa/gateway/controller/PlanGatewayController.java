91a2de8d446c96ba97dc73bfe79fd40c6f4f712e
RochQA <roch.sherriff@academytrainee.com>
2019-04-02 00:27:33 +0100

    refactored for plan tests

--- src/main/java/com/qa/gateway/controller/PlanGatewayController.java
+++ src/main/java/com/qa/gateway/controller/PlanGatewayController.java
@@ -1,7 +1,9 @@
 package com.qa.gateway.controller;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
@@ -19,6 +21,7 @@ import com.qa.gateway.entities.Account;
 import com.qa.gateway.entities.Constants;
 import com.qa.gateway.entities.CreateAccount;
 import com.qa.gateway.entities.Plan;
+import com.qa.gateway.repository.AccountRepository;
 import com.qa.gateway.service.PlanGatewayServiceImpl;
 @RestController
 public class PlanGatewayController {
@@ -26,31 +29,42 @@ public class PlanGatewayController {
 	private PlanGatewayServiceImpl srvc;
 	private RestTemplateBuilder rest;
 	private EurekaClient client;
+	@Autowired
+	AccountRepository repo;
 
 	public PlanGatewayController(PlanGatewayServiceImpl srvc, RestTemplateBuilder rest, EurekaClient client) {
 		this.srvc = srvc;
 		this.rest = rest;
 		this.client = client;
 	}
+	@PostMapping("/createAPlan")
+	public String createAPlan(@RequestBody Plan plan) {
+		repo.save(plan);
+		return "Saved";
+	}
 	@PostMapping("/createPlan")
 	public String createPlan(@RequestBody Plan plan) {
 		String checkRes = (checkPlan(plan));
 		if (checkRes.equals("Valid")) {
-			return savePlan(plan);
+//			return savePlan(plan);
+			repo.save(plan);
+			return "created";
 		}else return checkRes;
 	}
 	
 	@GetMapping("/getPlan/{planId}")
-	public Plan getPlan(@PathVariable Long planId) {
+	public Optional<Plan> getPlan(@PathVariable Long planId) {
 		HttpEntity<Long> entity = new HttpEntity<>(planId);
-		return this.rest.build().exchange(client.getNextServerFromEureka(Constants.GETTER, false).getHomePageUrl()+Constants.GET_PLAN_PATH, 
-				HttpMethod.GET, entity, Plan.class).getBody();
+//		return this.rest.build().exchange(client.getNextServerFromEureka(Constants.GETTER, false).getHomePageUrl()+Constants.GET_PLAN_PATH, 
+//				HttpMethod.GET, entity, Plan.class).getBody();
+		return repo.findById(planId);
 	}
 	
 	@GetMapping("/getAllPlans")
 	public List<Plan> getAllPlans(){
-		return this.rest.build().exchange(client.getNextServerFromEureka(Constants.GETTER, false).getHomePageUrl()+Constants.GET_ALL_PLAN_PATH, 
-				HttpMethod.GET, null, new ParameterizedTypeReference<List<Plan>>(){}).getBody();
+//		return this.rest.build().exchange(client.getNextServerFromEureka(Constants.GETTER, false).getHomePageUrl()+Constants.GET_ALL_PLAN_PATH, 
+//				HttpMethod.GET, null, new ParameterizedTypeReference<List<Plan>>(){}).getBody();
+		return repo.findAll();
 	
 	}
 	
@@ -62,19 +76,21 @@ public class PlanGatewayController {
 	
 	@DeleteMapping("/deletePlan/{planId}")
 	public String deletePlan(@PathVariable Long planId) {
-		HttpEntity<Long> entity = new HttpEntity<>(planId);
-		return this.rest.build().exchange(client.getNextServerFromEureka(Constants.GETTER, false).getHomePageUrl()+Constants.DELETE_PLAN_PATH, 
-				HttpMethod.DELETE, entity, String.class).getBody();
+//		HttpEntity<Long> entity = new HttpEntity<>(planId);
+//		return this.rest.build().exchange(client.getNextServerFromEureka(Constants.GETTER, false).getHomePageUrl()+Constants.DELETE_PLAN_PATH, 
+//				HttpMethod.DELETE, entity, String.class).getBody();
+		repo.deleteById(planId);
+		return "deleted";
 	}
 	private String checkPlan(Plan plan) {
 		HttpEntity<Plan> entity = new HttpEntity<>(plan);
-		return this.rest.build().exchange(client.getNextServerFromEureka(Constants.PLAN, false).getHomePageUrl()+Constants.CHECK_VALID_PATH, 
-				HttpMethod.PUT, entity, String.class).getBody();
+		return this.rest.build().exchange(client.getNextServerFromEureka(Constants.PLAN, false).getHomePageUrl()+Constants.CHECK_PLAN_PATH, 
+				HttpMethod.POST, entity, String.class).getBody();
 	}	
 	private String savePlan(Plan plan) {
-		HttpEntity<Plan> entity = new HttpEntity<>(plan);
-		this.rest.build().exchange(client.getNextServerFromEureka(Constants.GETTER, false).getHomePageUrl()+Constants.CREATE_PLAN_PATH, 
-				HttpMethod.POST, entity, String.class).getBody();
+//		HttpEntity<Plan> entity = new HttpEntity<>(plan);
+//		this.rest.build().exchange(client.getNextServerFromEureka(Constants.GETTER, false).getHomePageUrl()+Constants.CREATE_PLAN_PATH, 
+//				HttpMethod.POST, entity, String.class).getBody();
 		return Constants.VALID_MESSAGE;
 	}	
 
