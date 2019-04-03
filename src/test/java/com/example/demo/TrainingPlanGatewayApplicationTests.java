007c560852a4da3aaec662b2ccac5c1960e8c598
Dan-Malik <danial.malik1992@gmail.com>
2019-04-03 10:04:23 +0100

    Added annotation to test class

--- src/test/java/com/example/demo/TrainingPlanGatewayApplicationTests.java
+++ src/test/java/com/example/demo/TrainingPlanGatewayApplicationTests.java
@@ -5,8 +5,11 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.qa.gateway.GatewayApplication;
+import com.qa.trainer.TrainerApplication;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = GatewayApplication.class)
 public class TrainingPlanGatewayApplicationTests {
 
 	@Test
